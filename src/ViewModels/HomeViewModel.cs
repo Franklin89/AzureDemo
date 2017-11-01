24ca40bb22fa44e6c1f0b157f46b51dd50e7a1db
Matteo <matteolocher@gmail.com>
2017-11-01 21:18:09 +0100

    Update HomeViewModel.cs

--- src/ViewModels/HomeViewModel.cs
+++ src/ViewModels/HomeViewModel.cs
@@ -5,7 +5,7 @@ namespace AzureDemo.ViewModels
     public class HomeViewModel
     {
         public DateTime NextVisitDateUtc { get; set; }
-        public DateTime EndOfStayDateUtc => new DateTime(2017, 12, 22, 11, 0, 0, DateTimeKind.Utc);
+        public DateTime EndOfStayDateUtc => new DateTime(2017, 12, 15, 11, 0, 0, DateTimeKind.Utc);
         public string Title { get; set; }
 
         public HomeViewModel()
